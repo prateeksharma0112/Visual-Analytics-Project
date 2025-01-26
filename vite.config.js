522166c6c3b3ec0621f3476447cc9aae1ddb6e6b
prateek.sharma <prateeksharma0112@gmail.com>
2025-01-26 13:39:31 +0100

    added configuration for svelte pages to work in production

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+// import { sveltekit } from '@sveltejs/kit/vite';
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+// 	plugins: [sveltekit()]
+// });
 import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [svelte()],
+  base: 'https://prateeksharma0112.github.io/Visual-Analytics-Project/', // Replace with your GitHub repository name
 });
